a4f4beba54528a52c77c4c633a113cc9d9552eb5
lsgonc <120536540+lsgonc@users.noreply.github.com>
2025-03-25 14:01:21 -0300

    Testando código openmp com mais threads

--- openmp/script.bat
+++ openmp/script.bat
@@ -12,7 +12,7 @@ echo.
 
 echo Testes para N fixo (5000), K variavel e Threads variavel
 
-for %%t in (1 2 4 8) do (
+for %%t in (1 2 4 8 16 32) do (
     for %%k in (5 10 20 50 100) do (
         echo Testando N=5000, K=%%k, Threads=%%t
         for /L %%i in (1,1,5) do (
@@ -24,7 +24,7 @@ for %%t in (1 2 4 8) do (
 
 echo Testes para K fixo (10), N variavel e Threads variavel
 
-for %%t in (1 2 4 8) do (
+for %%t in (1 2 4 8 16 32) do (
     for %%n in (100 500 1000 5000) do (
         echo Testando N=%%n, K=10, Threads=%%t
         for /L %%i in (1,1,5) do (
